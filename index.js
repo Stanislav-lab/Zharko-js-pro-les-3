c2f07c331e17c3522d625c566d03771bd6d005b7
szharko <s_zharko@i.ua>
2023-02-06 23:27:24 +0200

    Fixed js file

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log("Like".concat("Java").concat("Script"))
-console.log("12345".split(""))
+let adr1 = prompt("Введите Ваш город");
+let adr2 = prompt("Введите название улицы");
+let adr3 = prompt("Введите номер дома");
+let result = adr1.concat(" ").concat(adr2).concat(" ").concat(adr3);
+
+console.log (result);
+alert(result);
+
+let num = prompt("Введите пятизначное число");
+let arr = num.split("");
+let total = arr.join(" ");
+
+console.log (total);
+alert(total);
+
+

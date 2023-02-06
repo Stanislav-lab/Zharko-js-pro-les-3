15e5a16d40d5db906d39b2f4a523a823909dacea
szharko <s_zharko@i.ua>
2023-02-06 23:52:15 +0200

    Added homework 4

---
+++ home-4.js
@@ -0,0 +1,11 @@
+let num1 = Number(prompt("Input first number"));
+let num2 = Number(prompt("Input second number"));
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let divi = num1 / num2;
+console.log(sum, sub, mul, divi);
+alert(`${num1} + ${num2} = ${sum}
+${num1} - ${num2} = ${sub}
+${num1} * ${num2} = ${mul}
+${num1} / ${num2} = ${divi}`);

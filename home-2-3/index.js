53445dfc110485a3cefa7db6cc546639377e1881
szharko <s_zharko@i.ua>
2023-02-14 00:09:49 +0200

    Added homework 11

--- home-2-3/index.js
+++ home-2-3/index.js
@@ -1,10 +1,10 @@
-// let adr1 = prompt("Введите Ваш город");
-// let adr2 = prompt("Введите название улицы");
-// let adr3 = prompt("Введите номер дома");
-// let result = adr1.concat(" ").concat(adr2).concat(" ").concat(adr3);
-//
-// console.log (result);
-// alert(result);
+let adr1 = prompt("Введите Ваш город");
+let adr2 = prompt("Введите название улицы");
+let adr3 = prompt("Введите номер дома");
+let result = adr1.concat(" ").concat(adr2).concat(" ").concat(adr3);
+
+console.log (result);
+alert(result);
 
 let num = prompt("Введите пятизначное число");
 let arr = num.split("");
